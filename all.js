30fe6df088f0b4a9d2f702bff8ff2f15d37a2d5b
ajmcsurley <mcsurley.4@osu.edu>
2016-03-27 21:25:47 -0400

    Check for empty textbox.

--- all.js
+++ all.js
@@ -5,6 +5,11 @@ function round(n, x) {
 function getResults() {
 	//Grab password
 	var pw = document.getElementById("pw-bar").value;
+	if (pw.length == 0){
+		document.getElementById("results").innerHTML = "";
+	}
+	else{
+
 	var result = zxcvbn(pw);
 
 	//Clear results container
@@ -17,4 +22,5 @@ function getResults() {
 	var resultsBox = document.getElementById("results");
 	resultsBox.innerHTML = " <table> <tbody> <tr> <td>Password: </td> <td>" + pw + "</td> </tr> <tr> <td>Your Score Is: </td> <td>" + result.score + "</td> </tr> <tr> <td>Time to Crack: </td> <td>" + result.calc_time + "</td> </tr> <tr> <td>Guesses: </td> <td>" + result.guesses + "</td> </tr> <tr> <td>Guesses_log10: </td> <td>" + result.guesses_log10 + "</td> </tr> <tr> <td>Guess Times: </td> </tr> <tr> <td>Online Throttled Attack: </td> <td>" + result.crack_times_display.online_throttling_100_per_hour + "</td> </tr> <tr> <td>Online, No Throttle Atack: </td> <td>" + result.crack_times_display.online_no_throttling_10_per_second + "</td> </tr> <tr> <td>Offline, Slow Hashing: </td> <td>" + result.crack_times_display.offline_slow_hashing_1e4_per_second + "</td> </tr> <tr> <td>Offline, Fast Hashing: </td> <td>" + result.crack_times_display.offline_fast_hashing_1e10_per_second + "</td> </tr> <tr> <td>Warning for Future Use (Score <= 2): </td> <td>" + result.feedback.warning + "</td> </tr> <tr> <td>Suggestions: </td> <td>" + result.feedback.suggestions + "</td> </tr> </tbody> </table>"
 	document.getElementById("results").innerHTML = resultsBox.innerHTML;
+	}
 }
