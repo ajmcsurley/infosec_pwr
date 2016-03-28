acd13cd2b6452e0c8de76ca442d24d8c39b1e07e
ajmcsurley <mcsurley.4@osu.edu>
2016-03-28 18:39:15 -0400

    Started patterns loop.

--- all.js
+++ all.js
@@ -31,12 +31,22 @@ function getResults() {
 
 	//Populate results
 	var resultsBox = document.getElementById("results");
-	resultsBox.innerHTML = " <table align=\"center\"> <tbody> <tr> <td>Password: </td> <td>" + pw + "</td> </tr> <tr> <td>Your Score Is: </td> <td>" + result.score + "</td> </tr> <tr> <td>Time to Crack: </td> <td>" + result.calc_time + "</td> </tr> <tr> <td>Guesses: </td> <td>" + result.guesses + "</td> </tr> <tr> <td>Guesses_log10: </td> <td>" + result.guesses_log10 + "</td> </tr> <tr> <td>Guess Times: </td> </tr> <tr> <td>Online Throttled Attack: </td> <td>" + result.crack_times_display.online_throttling_100_per_hour + "</td> </tr> <tr> <td>Online, No Throttle Atack: </td> <td>" + result.crack_times_display.online_no_throttling_10_per_second + "</td> </tr> <tr> <td>Offline, Slow Hashing: </td> <td>" + result.crack_times_display.offline_slow_hashing_1e4_per_second + "</td> </tr> <tr> <td>Offline, Fast Hashing: </td> <td>" + result.crack_times_display.offline_fast_hashing_1e10_per_second + "</td> </tr> <tr> <td>Warning for Future Use (Score <= 2): </td> <td>" + result.feedback.warning + "</td> </tr> <tr> <td>Suggestions: </td> <td>" + result.feedback.suggestions + "</td> </tr> </tbody> </table>"
+	resultsBox.innerHTML = " <table align=\"center\"> <tbody> <tr> <td>Password: </td> <td>" + pw + "</td> </tr> <tr> <td>Your Score Is: </td> <td>" + result.score + "</td> </tr> <tr> <td>Time to Crack: </td> <td>" + result.calc_time + "</td> </tr> <tr> <td>Guesses: </td> <td>" + result.guesses + "</td> </tr> <tr> <td>Guesses_log10: </td> <td>" + result.guesses_log10 + "</td> </tr> <tr> <td>Guess Times: </td> </tr> <tr> <td>Online Throttled Attack: </td> <td>" + result.crack_times_display.online_throttling_100_per_hour + "</td> </tr> <tr> <td>Online, No Throttle Atack: </td> <td>" + result.crack_times_display.online_no_throttling_10_per_second + "</td> </tr> <tr> <td>Offline, Slow Hashing: </td> <td>" + result.crack_times_display.offline_slow_hashing_1e4_per_second + "</td> </tr> <tr> <td>Offline, Fast Hashing: </td> <td>" + result.crack_times_display.offline_fast_hashing_1e10_per_second + "</td> </tr> <tr> <td>Warning for Future Use (Score <= 2): </td> <td>" + result.feedback.warning + "</td> </tr> <tr> <td>Suggestions: </td> <td>" + result.feedback.suggestions + "</td> </tr> <tr> <td>Pattern Matches: </td> </tr> </tbody> </table>"
 	
 	//Add button to chart
 	//resultsBox.innerHTML = resultsBox.innerHTML + "<input id=\"chart-btn\" type=\"button\" value=\"Chart It\">";
 
 	document.getElementById("results").innerHTML = resultsBox.innerHTML;
+
+	//Populate pattern matches
+	var patternsBox = document.getElementById("patterns");
+	console.log(result.sequence);
+	for (var i = 0; i < result.sequence.length; i++){
+		if(patterns.sequence[i]){
+			console.log(patterns.sequence[i].token);
+		}
+	}
+	//patternsBox.innerHTML = " <table align=\"center\"> <tbody> <tr> <td>"
 	}
 }
 
@@ -143,8 +153,8 @@ chartBtn.onclick = function() {
 	var c2 = result.crack_times_display.online_no_throttling_10_per_second;
 	var c2 = result.crack_times_display.offline_slow_hashing_1e4_per_second;
 	var c4 = result.crack_times_seconds.offline_fast_hashing_1e10_per_second;
-	guessesChart.addData([round(result.guesses_log10, 0)], pw.toString());
-	attacksChart.addData([round(c4, 0)], pw.toString());
+	guessesChart.addData([round(result.guesses_log10, 3)], pw.toString());
+	attacksChart.addData([round(c4, 3)], pw.toString());
 
 	//Re-renders the charts w/ updated values
 	guessesChart.update();
